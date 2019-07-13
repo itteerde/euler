51738a539ade26f970df9500720ae8b5cd4e820e
Erik Itter <erik.itter@gmail.com>
2019-07-13 02:36:51 +0200

    ...

---
+++ src/de/itter/euler/Problem0103.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.itter.euler;
+
+/**
+ * @author eriki
+ *
+ */
+public class Problem0103 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private boolean isValid(int[] a) {
+		return false;
+	}
+
+}
